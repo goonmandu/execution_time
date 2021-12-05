65f6580902c40f68d3750a06f8499d968d42cdcc
goonmandu <61984863+goonmandu@users.noreply.github.com>
2021-12-05 12:43:50 -0500

    Create boilerplate.py

---
+++ boilerplate.py
@@ -0,0 +1,17 @@
+from time import perf_counter_ns
+
+ITERATIONS = 10
+
+
+def main():
+    # main program code goes here
+
+
+if __name__ == "__main__":
+    exec_times = []
+    for i in range(ITERATIONS):
+        start = perf_counter_ns()
+        main()
+        stop = perf_counter_ns()
+        exec_times.append(stop - start)
+    print(f"Average execution time over 10 iterations: {(sum(exec_times) / len(exec_times)) / 1000000}ms")
